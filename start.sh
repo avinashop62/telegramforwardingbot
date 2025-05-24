935fc66812437f03cde55161794d2acb99891729
avinashop62 <avinashop62@gmail.com>
2025-05-24 23:18:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 source venv/bin/activate 2>/dev/null || echo "No venv found, continuing without it..."
-echo "Starting bot..."
-python3 control_bot.py
+echo "Starting Control Bot..."
+python3 control_bot.py &
+
+echo "Starting UserBot Manager..."
+python3 userbot_manager.py &
+wait
